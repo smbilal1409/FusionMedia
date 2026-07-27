78ecac6c9519c55601283f6cdba3f9fad1e8e7d7
smbilal1409 <smbilal1409@gmail.com>
2026-07-27 18:19:51 +0500

    adding search

--- Backend_setup-main/src/app.js
+++ Backend_setup-main/src/app.js
@@ -2,10 +2,7 @@ import express from "express"
 import cors from "cors";
 import cookieParser from "cookie-parser";
 const app=express();
-// app.use(cors({
-// origin:process.env.CORS_ORIGIN,
-// credentials:true
-// }))
+
 const allowedOrigins = process.env.CORS_ORIGIN.split(",");
 app.use(cors({
   origin: allowedOrigins,
